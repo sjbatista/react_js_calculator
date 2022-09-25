4051f022a2dd53e08686881bceb6120f2f523be2
sjbatista <salomaojunio@hotmail.com>
2022-09-25 13:39:57 -0300

    New component styles, new function 'logic calculator' and View alignment

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import { LogBox, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+import { LogBox, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, {useState, useEffect} from 'react';
-import CalcButton from './CalcButton'
+import CalcButton from './CalcButton';
 
 export default function App() {
 
@@ -18,24 +18,29 @@ for(var i = 0; i<=9; i++){
   numbers.push(i);
 }
 
+function logicCalculator(){
+  alert('lulu');
+}
+
   return (
     <View style={styles.container}>
+    <StatusBar hidden/>  
       
-      <View style={styles.topo}><Text style={{fontSize:24}}>{stringCalc}</Text></View>
+      <View style={styles.topo}><Text style={styles.textStringCalc}>{stringCalc}</Text></View>
 
-      <View style={{flex:1, flexDirection:'row'}}>
-        <TouchableHighlight style={{width:'25%'}}><Text style={{textAlign:'center', fontSize:24}}>+</Text></TouchableHighlight>
-        <TouchableHighlight style={{width:'25%'}}><Text style={{textAlign:'center', fontSize:24}}>-</Text></TouchableHighlight>
-        <TouchableHighlight style={{width:'25%'}}><Text style={{textAlign:'center', fontSize:24}}>/</Text></TouchableHighlight>
-        <TouchableHighlight style={{width:'25%'}}><Text style={{textAlign:'center', fontSize:24}}>x</Text></TouchableHighlight>
+      <View style={{flexDirection:'row', height:'16.6%',alignItems:'center'}}>
+        <TouchableOpacity style={styles.touchAbleOpacityStyle}><Text style={styles.textSignStyle}>+</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.touchAbleOpacityStyle}><Text style={styles.textSignStyle}>-</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.touchAbleOpacityStyle}><Text style={styles.textSignStyle}>/</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.touchAbleOpacityStyle}><Text style={styles.textSignStyle}>x</Text></TouchableOpacity>
       </View>
 
-      <View>
+      <View style={styles.viewNumbers}>
         {numbers.map(
           function(a){
             return(
-            <CalcButton number={a}></CalcButton>
-                  )
+            <CalcButton number={a} logicCalculator={logicCalculator}></CalcButton>
+                  );
             }
             )
         }
@@ -43,18 +48,48 @@ for(var i = 0; i<=9; i++){
 
     </View>
   );
-  
 }
- 
+
 const styles = StyleSheet.create({
   container: {
-    flex:1
+    flex:1,
+    backgroundColor:'black'
     
   },
 
   topo: {
     padding:10,
     borderBottomColor:'black',
-    borderBottomWidth:2
+    borderBottomWidth:2,
+    height:'16.6%',
+    justifyContent:'center',
+    paddingLeft:20,
   },
+
+  touchAbleOpacityStyle: {
+    width:'25%', 
+    backgroundColor:'rgb(20,20,20)',
+    height:'100%',
+    justifyContent:'center'
+  },
+
+  textSignStyle: {
+    textAlign:'center',
+    fontSize:24,
+    color:'white'
+  },
+
+  textStringCalc : {
+    fontSize:24,
+    color:'white',
+  },
+
+  viewNumbers: {
+    flexDirection:'row',
+    flexWrap:'wrap',
+    borderTopColor:'black',
+    borderTopWidth:2,
+    height:'66.8%'
+  }
 });
+
--- CalcButton.js
+++ CalcButton.js
@@ -1,4 +1,4 @@
-import { LogBox, StyleSheet, Text, View } from 'react-native';
+import { LogBox, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, {useState, useEffect} from 'react';
 
 export default function CalcButton(props) {
@@ -6,8 +6,16 @@ export default function CalcButton(props) {
     LogBox.ignoreAllLogs();
 
     return(
+        
+        <View style={{backgroundColor:'black', borderColor:'white', borderWidth:1, width:'33.3%', height:'25%'}}>
+        <TouchableOpacity onPress={() => props.logicCalculator()} style={{width: '100%', height:'100%', justifyContent:'center', alignItems:'center'}}>    
+        <Text style={{ fontSize:24, color:'white',}}>
+            {props.number.toString()}
+        </Text>
+        </TouchableOpacity>
 
-        <Text>{props.number.toString()}</Text>
+        </View>
+        
 
     );
 
